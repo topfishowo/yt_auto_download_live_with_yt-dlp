d7a86e2bff07c364fac57eaf8dd14ee7d507d4ec
鮪魚大師 <78526289+topfishowo@users.noreply.github.com>
2023-05-03 19:48:50 +0800

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -4,7 +4,7 @@ chcp 65001 > nul
 setlocal enabledelayedexpansion
 set "script=%~dp0Youtube_live_Auto_yt-dlp.py"
 
-title "Youtube 直播自動下載器 ver 1.1 by.鮪魚大師"
+title Youtube直播自動下載器by.鮪魚大師
 
 python "!script!"
 PAUSE
